4a8e7c5d234788fcb4dce02ff5070b8234116289
Jill Kuchman <jillkuchman@gmail.com>
2017-01-25 07:35:39 -0800

    Add firebase functionality

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { AlbumService } from '../album.service';
+import { Album } from '../album.model';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css'],
+  providers: [AlbumService]
+})
+export class AdminComponent implements OnInit {
+
+  constructor(private albumService: AlbumService) { }
+
+  ngOnInit() {
+  }
+
+  submitForm(title: string, artist: string, description: string){
+    debugger;
+    var newAlbum: Album = new Album(title, artist, description);
+    this.albumService.addAlbum(newAlbum);
+  }
+
+}
---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyDKm_rp3rwYsdgBbCKjR0pfumW51X37FJ8",
+  authDomain: "cd-store-epicodus.firebaseapp.com",
+  databaseURL: "https://cd-store-epicodus.firebaseio.com",
+  storageBucket: "cd-store-epicodus.appspot.com",
+  messagingSenderId: "978194993210"
+};
